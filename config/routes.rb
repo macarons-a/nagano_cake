f53f128826ca89da44a72393814afe3f8e343686
chip-k <129578647+chip-k@users.noreply.github.com>
2023-05-15 08:36:34 +0000

    [Add]routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,39 @@
 Rails.application.routes.draw do
+  
   devise_for :admins, skip: [:registrations, :passwords] , controllers: {
     sessions: "admin/sessions"
   }
 
   devise_for :customers,skip: [:passwords],  controllers: {
     registrations: "public/registrations",
-    sessions: 'public/sessions'
+    sessions:      'public/sessions'
   }
+  
+  namespace :admin do
+    get homes => 'admin/homes#top'
+    resources :products,    only: %i(index show new create edit update)
+    resources :genres,      only: %i(index create edit update)
+    resources :customers,   only: %i(index show edit update)
+    resources :orders,      only: %i(show update)
+    resources :order_items, only: %i(update)
+  end
+  
+  scope module: :public do
+    root :to                            => 'homes#top'
+    get homes                           => 'homes#about'
+    get    'customers/withdraw_confirm' => 'customers/withdraw_confirm'
+    patch  'customers/withdraw'         => 'customers/withdraw'
+    delete 'cart_items/destroy_all'     => 'cart_items#destroy_all'
+    post   'orders/confirm'             => 'orders#confirm'
+    get    'orders/complete'            => 'orders#complete'
+    resources :items,       only: %i(index show)
+    resources :customers,   only: %i(show edit updat)
+    resources :cart_items,  only: %i(index update destroy create)
+    resources :orders,      only: %i(new create index show)
+    resources :addresses,   only: %i(create index edit update destroy)
+  end
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
-  
   
 end
