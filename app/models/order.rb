d0bfd6ec2006d688673ba63baa0667321110325d
Momiji-otete <126852371+Momiji-otete@users.noreply.github.com>
2023-05-15 10:02:09 +0000

    [Add]enumの定義、ja.ymlの追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,14 @@
 class Order < ApplicationRecord
+  enum payment_method: {
+    credit_card: 0, #クレジットカード
+    transfer: 1 #銀行振り込み
+  }
+
+  enum status: {
+    waiting_for_payment: 0, #入金待ち
+    payment_confirmation: 1, #入金確認
+    is_making: 2, #製作中
+    preparing_ship: 3, #発送準備中
+    shipped: 4 #発送済み
+  }
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,2 +1,8 @@
 class OrderItem < ApplicationRecord
+  enum making_status: {
+    cannot_start: 0, #着手不可
+    wating_for_making: 1, #製作待ち
+    is_making: 2, #製作中
+    completion: 3 #製作完了
+  }
 end
