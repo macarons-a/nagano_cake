d5177fb0ec7df444cadbcdfe17084fdf310b0bbb
hitotmi <129574187+hitotmi@users.noreply.github.com>
2023-05-16 06:26:12 +0000

    [Add]customerモデル編集

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,9 +5,8 @@ class Customer < ApplicationRecord
   validates :last_name,presence:true
   validates :first_name_kana,presence:true
   validates :last_name_kana,presence:true
-  validates :adress,presence:true
+  validates :address,presence:true
   validates :postal_code,presence:true
-  validates :phone_number,presence:true
   validates :phone_number,presence:true
   validates :is_deleted,presence:true
 
