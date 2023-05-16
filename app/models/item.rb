141e0e01eb21299edda1583913c437ac5c210aa0
Momiji-otete <126852371+Momiji-otete@users.noreply.github.com>
2023-05-16 08:19:56 +0000

    [Add]adminのitemsコントローラー、viewの編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,17 @@ class Item < ApplicationRecord
   belongs_to :genre
   has_many :cart_items, dependent: :destroy
   has_many :oders, through: :order_items
+  has_one_attached :image
+
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :price, presence: true
+
+  def get_image
+    (image.attached?) ? image : 'no_image.jpg'
+  end
+
+  def with_tax_price
+    (price * 1.1).floor
+  end
 end
