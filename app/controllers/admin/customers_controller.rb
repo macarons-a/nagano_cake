65e6b70fdc78220d8506cabaffe081b1526e87b1
hitotmi <129574187+hitotmi@users.noreply.github.com>
2023-05-16 06:20:30 +0000

    [Add]会員編集画面

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,7 +1,7 @@
 class Admin::CustomersController < ApplicationController
 
   def index
-    @customer = Customer.all
+    @customers = Customer.all
   end
 
   def show
@@ -9,9 +9,23 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if  @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer.id)
+    else
+      render:edit
+    end
   end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :phone_number, :email, :is_deleted)
+  end
+
 
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,9 +1,21 @@
 class Customer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :first_name,presence:true
+  validates :last_name,presence:true
+  validates :first_name_kana,presence:true
+  validates :last_name_kana,presence:true
+  validates :adress,presence:true
+  validates :postal_code,presence:true
+  validates :phone_number,presence:true
+  validates :phone_number,presence:true
+  validates :is_deleted,presence:true
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :cart_items, dependent: :destroy
   has_many :addresses, dependent: :destroy
   has_many :orders, dependent: :destroy
+
 end
