18797f3ab8a153d3a2b19f3baef08ee4f7bc4c01
hitotmi <129574187+hitotmi@users.noreply.github.com>
2023-05-17 13:12:41 +0000

    [Add]注文情報入力画面

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -2,9 +2,11 @@ class Public::OrdersController < ApplicationController
 
   def new
     @order = Order.new
+    @customer = current_customer
+    @addresses = @customer.addresses
   end
   def create
-    cart_items = current_customer.cart_items.all
+    cart_items = current_customer.cart_items
     @order = current_customer.orders.new(order_params)
     if @order.save
       cart_items.each do |cart|
@@ -12,10 +14,10 @@ class Public::OrdersController < ApplicationController
         order_item.item_id = cart.item_id
         order_item.order_id = @order.id
         order_item.order_quantity = cart.quantity
-        order_item.order_price = cart.item.price
+        order_item.order_price = cart.item.price # 税込み？税抜き？
         order_item.save
       end
-      redirect_to
+      redirect_to　order
       cart_items.destroy_all
     else
       @order = Order.new(order_params)
