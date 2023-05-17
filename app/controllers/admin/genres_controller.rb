09b929075c060ae13692616c31eca9ec31a9c0fb
chip-k <129578647+chip-k@users.noreply.github.com>
2023-05-17 11:05:30 +0000

    [Fix/Add]pulic/customer

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -19,9 +19,12 @@ class Admin::GenresController < ApplicationController
   
   def update
     genre = Genre.find(params[:id])
-    genre.update(genre_params)
-    flash[:success] = "ジャンルを編集しました。"
-    redirect_to admin_genres_path
+    if genre.update(genre_params)
+      flash[:success] = "ジャンルを編集しました。"
+      redirect_to admin_genres_path
+    else
+      render :edit
+      
   end
   
   private
