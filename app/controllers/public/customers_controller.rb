d796a9dafc3da4183dc34138d8ecbecb6b736288
hitotmi <129574187+hitotmi@users.noreply.github.com>
2023-05-17 07:02:30 +0000

    [Add]会員編集画面

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -5,9 +5,17 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = current_customer
   end
 
   def update
+    @customer = current_customer
+    if  @customer.update(customer_params)
+      flash[:notice] = "会員情報を変更しました"
+      redirect_to customers_path
+    else
+      render:edit
+    end
   end
 
   def withdraw_confirm
@@ -16,4 +24,10 @@ class Public::CustomersController < ApplicationController
   def withdraw
   end
 
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :phone_number, :email, :is_deleted)
+  end
 end
