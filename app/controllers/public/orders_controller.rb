22b46ee0383a3bfe88d4570da6a4d5fa03192c00
hitotmi <129574187+hitotmi@users.noreply.github.com>
2023-05-17 11:22:43 +0000

    [Add]注文情報入力画面

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,2 +1,45 @@
 class Public::OrdersController < ApplicationController
+
+  def new
+    @order = Order.new
+  end
+  def create
+    cart_items = current_customer.cart_items.all
+    @order = current_customer.orders.new(order_params)
+    if @order.save
+      cart_items.each do |cart|
+        order_item = OrderItem.new
+        order_item.item_id = cart.item_id
+        order_item.order_id = @order.id
+        order_item.order_quantity = cart.quantity
+        order_item.order_price = cart.item.price
+        order_item.save
+      end
+      redirect_to
+      cart_items.destroy_all
+    else
+      @order = Order.new(order_params)
+      render :new
+    end
+  end
+
+
+  def confirm
+  end
+
+
+  def index
+  end
+
+  def show
+  end
+
+  def complete
+  end
+
+  def order_params
+    params.require(:order).permit(:postal_code,:address, :name, :shipping_cost, :total_payment, :payment_method,:customer_id, :status)
+  end
+
+
 end
