3c674e6d44325fe52f41d082d129d4bfd2f09c55
chip-k <129578647+chip-k@users.noreply.github.com>
2023-05-18 07:23:03 +0000

    [Add]admin/order

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -1,2 +1,21 @@
 class Admin::OrderItemsController < ApplicationController
+  
+  def update
+    order = Order.find(params[:id])
+    order.order_items.each do |order_item|
+      if order_item.update(order_item_params)
+        flash[:success] = "ステータスを変更しました。"
+        redirect_to admin_orders_path
+      else
+        render :show
+      end
+    end
+  end
+  
+  private
+  
+  def order_item_params
+    params.require(:order).permit(order_items_attributes: [:making_status])
+  end
+  
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,31 +1,28 @@
 class Admin::OrdersController < ApplicationController
   
   def index
-    @order = Order.all
+    @orders = Order.page(params[:page])
   end
   
   def show
     @order = Order.find(params[:id])
+    @total_payment = @order.pirce.subtotal
+    @shipping_cost = 800
   end
   
   def update
     order = Order.find(params[:id])
-    order.update(order_params)
-    order.order_items.each do |order_item|
-      order_item.update(order_item_params)
-    end
-    flash[:success] = "ステータスを変更しました。"
-    redirect_to admin_orders_path
+    if order.update(order_params)
+      flash[:success] = "ステータスを変更しました。"
+      redirect_to admin_orders_path
+    else
+      render :show
   end
   
   private
   
   def order_params
     params.require(:order).permit(:status)
-  end
-  
-  def order_item_params
-    params.require(:order).permit(order_items_attributes: [:making_status])
   end
   
 end
