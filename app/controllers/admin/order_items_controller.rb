81ae51ab74675d0942e2e6577623991ffaa5a583
hitotmi <129574187+hitotmi@users.noreply.github.com>
2023-05-20 05:31:30 +0000

    [Add]注文確認画面

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -1,6 +1,6 @@
 class Admin::OrderItemsController < ApplicationController
   before_action :authenticate_admin!
-  
+
   def update
     order_item = OrderItem.find(params[:id])
     order = order_item.order
