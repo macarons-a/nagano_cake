b08dede9e962a44dd1d8cde38cece181b8ba034b
Momiji-otete <126852371+Momiji-otete@users.noreply.github.com>
2023-05-25 06:40:24 +0000

    [Update]画像のリサイズをした

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,8 +8,12 @@ class Item < ApplicationRecord
   validates :introduction, presence: true
   validates :price, presence: true
 
-  def get_image
-    (image.attached?) ? image : 'no_image.jpg'
+  def get_image(width, height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image.variant(resize_to_limit: [width, height]).processed
   end
 
   def with_tax_price
